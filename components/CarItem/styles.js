babc214ffb57a37e75150440ae4c555fbc709f88
Baux Aurélien <baux.aurelien62@gmail.com>
2021-02-23 20:47:18 +0100

    Réalisation d'une fausse application Tesla

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,28 +1,44 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
+const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
     carContainer: {
         width: '100%',
-        height: '100%',
+        height: windowHeight,
     },
+
     titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
+        marginTop: '30%',
+        width: '100%',
+        alignItems: 'center',
     },
+
     title: {
-    fontSize: 40,
-    fontWeight: 'bold',
+        fontSize: 40,
+        fontWeight: 'bold',
     },
+
+    subtileCTA: {
+        textDecorationLine: 'underline',
+        
+    },
+
     subtitle: {
-    fontSize: 16,
-    color: '#5c5e62'
+        fontSize: 16,
+        color: '#5c5e62'
     },
+
     image: {
-    width: '100%',
-    height:'100%',
-    resizeMode: 'cover',
-    position: 'absolute',
+        width: '100%',
+        height:'100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+    },
+
+    buttonsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '100%',
     }
 });
 
---
+++ components/CarsList/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, FlatList, Dimensions} from 'react-native';
+import CarItem from '../CarItem';
+
+import styles from './styles';
+import cars from './cars';
+
+const windowHeight = Dimensions.get('window').height;
+const CarsList = (props) => {
+    return (
+        <View style={styles.container}>
+            <FlatList 
+                data={cars}
+                renderItem={({item}) => <CarItem car={item} />}
+                showsVerticalScrollIndicator={false}
+                snapToAlignment={'start'}
+                decelerationRate={'fast'}
+                snapToInterval={windowHeight}
+            />
+        </View>
+    );
+};
+
+export default CarsList;
